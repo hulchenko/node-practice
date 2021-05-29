c3cc148fdcb03deb6a39481d8e3fb1b792a429d1
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-28 22:55:20 -0600

    init

---
+++ i_o.js
@@ -0,0 +1,25 @@
+// blocking
+const getUserSync = require('.path/getUserSync');
+
+const userOne = getUserSync(1);
+console.log(userOne);
+
+const userOne = getUserSync(1);
+console.log(userOne);
+
+const sum = 2 + 2;
+console.log(sum);
+
+// non-blocking
+const getUserSync = require('.path/getUserSync');
+
+getUserSync(1, (user) => {
+  console.log(user);
+});
+
+getUserSync(2, (user) => {
+  console.log(user);
+});
+
+const sum = 2 + 2;
+console.log(sum);
